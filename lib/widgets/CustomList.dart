66b408289d1ac5af895f7f83cf95683818b3a93f
Attilio Oliva <attilio.oliva@protonmail.com>
2021-04-02 16:32:46 +0200

    Decoupled selection frame and list widget

---
+++ lib/widgets/CustomList.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class Item {
+  Item(this.title, this.colorValue);
+  String title;
+  int colorValue;
+}
+
+class CustomList extends StatefulWidget {
+  CustomList({Key key, this.items}) : super(key: key);
+
+  final List<Item> items;
+
+  @override
+  State<StatefulWidget> createState() => _ReordableState();
+}
+
+class _ReordableState extends State<CustomList> {
+  void reorderData(int oldindex, int newindex) {
+    setState(() {
+      if (newindex > oldindex) {
+        newindex -= 1;
+      }
+      final item = widget.items.removeAt(oldindex);
+      widget.items.insert(newindex, item);
+    });
+  }
+
+  void sorting() {
+    setState(() {
+      widget.items.sort();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ReorderableListView(
+      children: <Widget>[
+        for (final item in widget.items)
+          ListTile(
+            key: ValueKey(item.title),
+            title: Text(item.title),
+            leading: Icon(Icons.book, color: Color(item.colorValue)),
+          )
+      ],
+      onReorder: reorderData,
+    );
+  }
+}
--- lib/widgets/ListSelection.dart
+++ lib/widgets/ListSelection.dart
@@ -1,32 +1,20 @@
 import 'package:flutter/material.dart';
+import 'CustomList.dart';
 
 class ListSelection extends StatefulWidget {
   ListSelection({Key key, this.title}) : super(key: key);
 
   final String title;
-  final List<String> items = ["University", "Work", "Memos"];
-
+  final List<Item> items = [
+    Item("University", 0xffe040fb),
+    Item("Work", 0xff448aff),
+    Item("Memos", 0xffeeff41),
+  ];
   @override
-  _NotebookSelection createState() => _NotebookSelection();
+  _NotebookState createState() => _NotebookState();
 }
 
-class _NotebookSelection extends State<ListSelection> {
-  void reorderData(int oldindex, int newindex) {
-    setState(() {
-      if (newindex > oldindex) {
-        newindex -= 1;
-      }
-      final String item = widget.items.removeAt(oldindex);
-      widget.items.insert(newindex, item);
-    });
-  }
-
-  void sorting() {
-    setState(() {
-      widget.items.sort();
-    });
-  }
-
+class _NotebookState extends State<ListSelection> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,17 +22,7 @@ class _NotebookSelection extends State<ListSelection> {
         title: Text(widget.title),
         centerTitle: true,
       ),
-      body: ReorderableListView(
-        children: <Widget>[
-          for (final String item in widget.items)
-            ListTile(
-              key: ValueKey(item),
-              title: Text(item),
-              leading: Icon(Icons.book),
-            ),
-        ],
-        onReorder: reorderData,
-      ),
+      body: CustomList(items: widget.items),
       bottomNavigationBar: BottomAppBar(
         child: Row(children: [
           Spacer(),
